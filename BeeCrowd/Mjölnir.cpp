dc79c476532c8e14f95dd50ce79c06fc24e35862
Md Raihan Chowdhoury <mdr8077@gmail.com>
2024-04-27 01:42:59 +0600

    Last

---
+++ BeeCrowd/Mjölnir.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    while(n--)
+    {
+        string a;
+        int b;
+        cin >> a >> b;
+        if(a=="Thor") cout << "Y" << endl;
+        else cout << "N" << endl;
+    }
+    return 0;
+}
